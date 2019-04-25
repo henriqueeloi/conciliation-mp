ecfeeaa5d4bfddd9bb09167516cb77d69df43691
heloi <henrique.eloi@mercadolivre.com>
2019-04-25 08:54:21 -0300

    Ajustes treiler do arquivo

--- src/main/java/com/mercadopago/basedois/ComprovanteVendaTreiler.java
+++ src/main/java/com/mercadopago/basedois/ComprovanteVendaTreiler.java
@@ -3,6 +3,9 @@ package com.mercadopago.basedois;
 import com.github.ffpojo.file.processor.FlatFileProcessor;
 import com.github.ffpojo.file.processor.record.RecordProcessor;
 import com.github.ffpojo.file.processor.record.handler.ErrorHandler;
+import net.sf.jsefa.Serializer;
+import net.sf.jsefa.flr.FlrIOFactory;
+import net.sf.jsefa.flr.config.FlrConfiguration;
 import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.annotation.BeforeStep;
 import org.springframework.batch.core.listener.StepExecutionListenerSupport;
@@ -10,6 +13,7 @@ import org.springframework.batch.item.ExecutionContext;
 import org.springframework.batch.item.file.FlatFileFooterCallback;
 
 import java.io.IOException;
+import java.io.StringWriter;
 import java.io.Writer;
 
 public class ComprovanteVendaTreiler  extends StepExecutionListenerSupport implements FlatFileFooterCallback {
@@ -21,15 +25,57 @@ public class ComprovanteVendaTreiler  extends StepExecutionListenerSupport imple
     public void writeFooter(Writer writer) throws IOException {
 
         ExecutionContext stepContext = this.stepExecution.getExecutionContext();
-        
+        Integer totalRegs = new Integer(stepExecution.getWriteCount());
 
-        writer.write("# Write count: "
-            + stepExecution.getWriteCount());
+        //Sub Footer
 
-        writer.write(System.getProperty("line.separator"));
+         writer.write(getSubFooter(writer, totalRegs));
 
-        long delta = stepExecution.getEndTime().getTime();
-        writer.write("# Done in: " + delta + "ms");
+
+        //Footer
+
+        writer.write(getFooter(writer, totalRegs));
+
+    }
+
+    private String getFooter(Writer writer, Integer totalRegs) {
+
+        FlrConfiguration config = new FlrConfiguration();
+        config.setLineBreak("");
+
+        Serializer serializer = FlrIOFactory.createFactory(config, SettlementFooterFileDTO.class).createSerializer();
+        StringWriter stringWriter = new StringWriter();
+
+        totalRegs += 4;
+        SettlementFooterFileDTO footerFileDTO = new SettlementFooterFileDTO();
+        footerFileDTO.setTotalGeralRegistros(String.valueOf(totalRegs));
+
+
+        footerFileDTO.setNseq(totalRegs.toString());
+
+        serializer.open(stringWriter);
+        serializer.write(footerFileDTO);
+
+        return stringWriter.toString();
+    }
+
+    private String getSubFooter(Writer writer, Integer totalRegs) throws IOException {
+
+        Serializer serializer = FlrIOFactory.createFactory(SettlementFooterLoteDTO.class).createSerializer();
+        StringWriter stringWriter = new StringWriter();
+        totalRegs += 3;
+
+        SettlementFooterLoteDTO footerFileDTO = new SettlementFooterLoteDTO();
+        footerFileDTO.setTotalRegistrosTransacoes(totalRegs.toString());
+        footerFileDTO.setTotalValoresCreditos("530");
+
+
+        footerFileDTO.setNseq(totalRegs.toString());
+
+        serializer.open(stringWriter);
+        serializer.write(footerFileDTO);
+
+        return stringWriter.toString();
     }
 
     @BeforeStep
